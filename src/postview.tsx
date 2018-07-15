8f0c6c88ad7ef46cb465151197374db4733d3abd
Sreyas <Sreyassreelal@gmail.com>
2018-07-15 09:31:21 +0000

    styled postview

--- src/postview.tsx
+++ src/postview.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import cookies from 'react-cookies';
-import { END_POINT } from './App';
+import { END_POINT, COOKIE_OPTIONS } from './App';
+import { Button, Classes} from '@blueprintjs/core';
 
 interface PostContentProps{
     match: { params: { PostID: string; } };
@@ -14,14 +15,33 @@ export class PostView extends React.Component<PostContentProps,any>{
             error:'',
             loggedin:cookies.load("loggedin")
         }
+        this.DeletePost = this.DeletePost.bind(this)
         this.GetContentById()
+    }
+    async DeletePost(){
+        let formData = new FormData(),response:Response;
+        formData.append('PostID', this.props.match.params.PostID);
+        response = await fetch(END_POINT + "/v1/delete",{
+            body:formData,    
+            method:'post',
+            credentials: 'include',
+            mode: 'cors',
+            headers: {
+                'Authorization':'Basic '+btoa(cookies.load('name',COOKIE_OPTIONS)+':'+cookies.load('password',COOKIE_OPTIONS))
+            }
+        })
+        if(response.ok){
+            location.href="/"
+        }else{
+            console.log("Delete failed")
+            this.setState({error:"Server couldn't delete that post now "})
+        }
     }
     async GetContentById(){
         let response:Response;
         console.log("Post id is %s ",this.props.match.params.PostID)
         response = await fetch(END_POINT+"/v1/getpostbyid/" + this.props.match.params.PostID,{
                                 method:'get',
-    
         });
 
         if(response.ok){
@@ -39,12 +59,22 @@ export class PostView extends React.Component<PostContentProps,any>{
             <div id="contentview">
                 {this.state.error === '' ? (
                     <div>
-                        <h1>
+                        <div id="title">
                             {this.state.title}
-                        </h1>
-                        <p>
+                        </div>
+                        <div id="content">
                             {this.state.content}
-                        </p>
+                        </div>
+                        {this.state.loggedin ?(
+                            <Button 
+                                className={Classes.INTENT_DANGER} 
+                                text="Delete" 
+                                onClick={this.DeletePost}
+                            />
+                            ):(
+                                <div/>
+                        )}
+
                     </div>
                 ):(
                     <div id="error">
