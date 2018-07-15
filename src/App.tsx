2509509a7a6795a33be9a1a8503bed43f671fb79
Sreyas <Sreyassreelal@gmail.com>
2018-07-15 03:37:36 +0000

    use exact ipv4 address instead of loopback ip to improve performance of nginx reverse proxy

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import {Home} from './home';
 import cookie from 'react-cookies';
 import { PostForm } from './postform';
 
-export const API_HOST  = "localhost";
+export const API_HOST  = "192.168.1.2"; //I used exact ipv4 to speed up nginx reverse proxy.Please change it according 
 export const	API_PORT = "8000";
 export const END_POINT = "http://" + API_HOST + ":" + API_PORT;
 
@@ -28,9 +28,7 @@ class App extends React.Component<any,any>{
     super(props)
     this.state={
       loggedin:cookie.load("loggedin"),
-    
     };
-    
   }
 
   OnUserLoggedIn(username:string,password:string){
@@ -74,6 +72,9 @@ class App extends React.Component<any,any>{
           </NavbarGroup>
         
         </Navbar>
+        <h1 className={Classes.TEXT_MUTED} id="bigheader">
+              Express-Blog
+        </h1>
         {this.state.loggedin === undefined ?(
           <Route
             path="/login"
